fac19ba64850ab9e9c6fd133c7ae9a84f10f4ec5
MaorMa <maimonmaor@gmail.com>
2018-10-18 14:25:29 +0300

    adding insert and select queries from db

--- src/Controller/DBconnection.java
+++ src/Controller/DBconnection.java
@@ -20,9 +20,28 @@ public class DBconnection {
 
     }
 
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:users.db";
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS users (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public void insert(String name, double capacity) {
-        String createTable="CREATE TABLE users(UserName, Password)";
-        String sql = "INSERT INTO users(UserName, Password) VALUES(?,?)";
+        String sql = "INSERT INTO users(name,capacity) VALUES(?,?)";
 
         try (Connection conn = this.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -34,9 +53,28 @@ public class DBconnection {
         }
     }
 
+    public void selectAll(){
+        String sql = "SELECT id, name, capacity FROM users";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         DBconnection d=new DBconnection();
+        d.createNewTable();
         d.insert("Raw Materials", 3000);
-
+        d.selectAll();
     }
 }
