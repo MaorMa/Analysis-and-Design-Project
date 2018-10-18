66fb742c85cf275d007a1092da8d675f48a0f3c6
MaorMa <maimonmaor@gmail.com>
2018-10-18 13:05:42 +0300

    add fxml files

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../View/Home.fxml"));
+        primaryStage.setTitle("Vacation4U");
+        primaryStage.setScene(new Scene(root, 600  , 600));
         primaryStage.show();
     }
 
