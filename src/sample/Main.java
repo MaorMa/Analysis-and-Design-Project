517e77d8e6d1ee409d6d6973b0ab9712f1cd0fb4
saarm <saarm24@gmail.com>
2018-10-18 13:30:35 +0300

    test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../View/Home.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../View/Login.fxml"));
         primaryStage.setTitle("Vacation4U");
         primaryStage.setScene(new Scene(root, 600  , 600));
         primaryStage.show();
