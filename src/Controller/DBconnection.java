ba608214a560179cc0f156c066809fbae5b57f53
MaorMa <maimonmaor@gmail.com>
2018-10-18 14:11:06 +0300

    testing db

--- src/Controller/DBconnection.java
+++ src/Controller/DBconnection.java
@@ -6,9 +6,8 @@ package Controller;
 import java.sql.*;
 
 public class DBconnection {
-    public static void main( String args[] ) {
+    private Connection connect() {
         Connection c = null;
-
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:users.db");
@@ -17,5 +16,27 @@ public class DBconnection {
             System.exit(0);
         }
         System.out.println("Connection established successfully");
+        return c;
+
+    }
+
+    public void insert(String name, double capacity) {
+        String createTable="CREATE TABLE users(UserName, Password)";
+        String sql = "INSERT INTO users(UserName, Password) VALUES(?,?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        DBconnection d=new DBconnection();
+        d.insert("Raw Materials", 3000);
+
     }
 }
