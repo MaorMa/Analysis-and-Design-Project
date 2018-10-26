c5a1263b30088090e186b508454df755fc35f719
avivc411 <avivc411@gmail.com>
2018-10-26 16:36:13 +0300

    Enter instead of pressing button function in some forms

--- src/main/java/View/UpdateView.java
+++ src/main/java/View/UpdateView.java
@@ -6,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -47,22 +49,40 @@ public class UpdateView implements Initializable{
         setCurrentUserDetails();
     }
 
-    public void updateUserInfo(ActionEvent actionEvent) throws InterruptedException {
+    public void updateUserInfo() throws InterruptedException {
         String date_to_string = "";
-        String oldPassword = updateController.getUser().getPassword();//todo need to check what if pw is empty!!!
+        //String oldPassword = updateController.getUser().getPassword();//todo need to check what if pw is empty!!!
         if(date.getValue() != null)//check if date is null
         {
             date_to_string = date.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));//problem when empty!
         }
-        updateController.updateUserInfo(username.getText(), password.getText(), date_to_string, fname.getText(),
-                lname.getText(), city.getText());
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Update Confirmation");
-        alert.setHeaderText("Update Done");
+        if(password.getText().equals("") || fname.getText().equals("") || lname.getText().equals("") ||
+                city.getText().equals("") || date_to_string.equals("")){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Update Error");
+            alert.setHeaderText("One of the fields is empty");
 
-        alert.showAndWait();
-        Stage stage = (Stage) updateUserButton.getScene().getWindow();
-        stage.close();
+            alert.showAndWait();
+        }
+        else {
+            updateController.updateUserInfo(username.getText(), password.getText(), date_to_string, fname.getText(),
+                    lname.getText(), city.getText());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Update Confirmation");
+            alert.setHeaderText("Update Done");
+
+            alert.showAndWait();
+            Stage stage = (Stage) updateUserButton.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    public void onEnter(KeyEvent keyEvent) throws InterruptedException {
+        if (keyEvent.getCode().equals(KeyCode.ENTER))
+        {
+            Thread.sleep(500);
+            this.updateUserInfo();
+        }
     }
 
 }
